d14756ae8767b6217f0c4c4a27c1523190167f1c
johnahumanbeing <mseewak@gmail.com>
2024-01-16 22:53:37 +0300

    state model update

--- models/state.py
+++ models/state.py
@@ -1,8 +1,32 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models import storage_type
+from models.city import City
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
+
 
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+
+    if storage_type == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state",
+                              cascade="all, delete")
+    else:
+        name = ""
+
+        @property
+        def cities(self):
+            """
+            It will be the FileStorage relationship between State and
+            City
+            """
+            from models import storage
+
+            all_cities = storage.all(City)
+            return [city for city in all_cities.values()
+                    if city.state_id == self.id]
