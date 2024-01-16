715419d2032d6c509c16810b1997722be4297d7a
johnahumanbeing <mseewak@gmail.com>
2024-01-16 22:54:15 +0300

    user model updated

--- models/user.py
+++ models/user.py
@@ -1,11 +1,28 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models.place import Place
+from models.review import Review
+from models import storage_type
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
 
 
 class User(BaseModel):
     """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    __tablename__ = 'users'
+
+    if storage_type != 'db':
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
+    else:
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship('Place', backref='user',
+                              cascade='all, delete')
+        reviews = relationship('Review', backref='user',
+                               cascade='all, delete')
